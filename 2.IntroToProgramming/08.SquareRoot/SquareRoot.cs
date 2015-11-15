72e98559c428261f27f4947e4e104798c3069174
Martin Dobrudzhanski <martin.dobr@abv.bg>
2015-11-15 11:06:10 +0200

    homework

---
+++ 2.IntroToProgramming/08.SquareRoot/SquareRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+        Console.WriteLine(Math.Sqrt((double)12345));
+    }
+}
+
---
+++ 2.IntroToProgramming/14.CurrentDateTime/CurrentDateTime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+class CurrentDateTime
+{
+    static void Main()
+    {
+        DateTime now = DateTime.Now;
+        Console.WriteLine(now);
+    }
+}
+
