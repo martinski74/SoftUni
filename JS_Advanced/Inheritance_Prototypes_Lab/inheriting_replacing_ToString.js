c5dab0497227c2d7709e083210db64920334bcdd
martinski74 <martin.dobr@abv.bg>
2016-11-02 19:03:14 +0200

    work in class

---
+++ JS_Advanced/Inheritance_Prototypes_Lab/inheriting_replacing_ToString.js
@@ -0,0 +1,48 @@
+function toStringExtension() {
+
+    class Person {
+        constructor(name, email) {
+            this.name = name;
+            this.email = email;
+        }
+        toString() {
+            let className = this.constructor.name;
+            return `${className} (name: ${this.name}, email: ${this.email})`;
+        }
+    }
+    class Teacher extends Person {
+        constructor(name, email, subject) {
+            super(name, email);
+            this.subject = subject;
+        }
+        toString() {
+            let baseStr = super.toString().slice(0, -1);
+            return baseStr + `, subject: ${this.subject})`;
+        }
+    }
+    class Student extends Person {
+        constructor(name, email, course) {
+            super(name, email);
+            this.course = course;
+        }
+        toString() {
+            let baseStr = super.toString().slice(0, -1);
+            return baseStr + `, course: ${this.course})`;
+        }
+    }
+    return {
+        Person,
+        Teacher,
+        Student
+    }
+}
+
+
+
+
+
+let p = new Person("Maria", "maria@gmail.com");
+console.log('' + p);
+let t = new Teacher("Ivan", "iv@yahoo.com", "PHP");
+console.log('' + t);
+
