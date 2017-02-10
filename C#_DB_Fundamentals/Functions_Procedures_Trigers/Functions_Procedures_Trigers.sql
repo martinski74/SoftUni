cc2697a3fdda9f34ad68c0fb006b3cef061dddd7
martinski74 <martin.dobr@abv.bg>
2017-02-10 12:13:57 +0200

    update

--- C#_DB_Fundamentals/Functions_Procedures_Trigers/Functions_Procedures_Trigers.sql
+++ C#_DB_Fundamentals/Functions_Procedures_Trigers/Functions_Procedures_Trigers.sql
@@ -1,164 +1,246 @@
--- Problem 1. Employees with Salary Above 35000
-CREATE PROCEDURE usp_GetEmployeesSalaryAbove35000 
+-- 01. Employees with Salary Above 35000
+CREATE PROCEDURE usp_GetEmployeesSalaryAbove35000
 AS
 BEGIN
- SELECT FirstName, LastName FROM [dbo].[Employees] AS e 
-	WHERE e.Salary > 35000
+	SELECT FirstName AS 'First Name', LastName AS 'Last Name'
+	FROM Employees
+	WHERE Salary > 35000
 END
 
--- Problem 2. Employees with Salary Above Number
-CREATE PROCEDURE usp_GetEmployeesSalaryAboveNumber (@LowerBound MONEY)
+EXEC usp_GetEmployeesSalaryAbove35000
+
+-- 02. Employees with Salary Above Number
+CREATE PROCEDURE usp_GetEmployeesSalaryAboveNumber (@number Money)
 AS
 BEGIN
- SELECT FirstName, LastName FROM [dbo].[Employees] AS e 
-	WHERE e.Salary >= @LowerBound
+	SELECT FirstName AS 'First Name', LastName AS 'Last Name'
+	FROM Employees
+	WHERE Salary >= @number
 END
 
--- Problem 3. Town Names Starting With
-CREATE PROCEDURE usp_GetTownsStartingWith (@prefix VARCHAR(MAX))
+EXEC usp_GetEmployeesSalaryAboveNumber 48100
+
+-- 03. Town Names Starting With
+CREATE PROCEDURE usp_GetTownsStartingWith (@parameter NVARCHAR(30))
 AS
 BEGIN
- SELECT Name FROM [dbo].Towns AS e 
-	WHERE Name LIKE CONCAT(@prefix, '%')
+	SELECT Name AS Town FROM Towns
+	WHERE Name LIKE @parameter + '%'
 END
 
--- Problem 4. Employees from Town
-CREATE PROCEDURE usp_GetEmployeesFromTown  (@TownName VARCHAR(MAX))
+EXEC usp_GetTownsStartingWith b
+
+-- 04. Employees from Town
+CREATE PROCEDURE usp_GetEmployeesFromTown (@townName NVARCHAR(30))
 AS
 BEGIN
- SELECT e.FirstName, e.LastName FROM Employees AS e 
-	LEFT JOIN Addresses AS a ON a.AddressID = e.AddressID
-	LEFT JOIN Towns AS t ON t.TownID = a.TownID
-	WHERE t.Name = @TownName
+	SELECT e.FirstName AS 'First Name', e.LastName AS 'Last Name'
+	FROM Employees e
+	JOIN Addresses a
+	ON e.AddressID = a.AddressID
+	JOIN Towns t
+	ON t.TownID = a.TownID
+	WHERE t.Name = @townName
 END
 
--- Problem 5.	Salary Level Function
+EXEC usp_GetEmployeesFromTown Sofia
+
+-- 05. Salary Level Function
 CREATE FUNCTION ufn_GetSalaryLevel(@salary MONEY)
-RETURNS VARCHAR(10)
+RETURNS NVARCHAR(10)
 AS
 BEGIN
-	DECLARE @Result VARCHAR(10)
-	IF (@salary < 30000)
-	BEGIN
-		SET @Result = 'Low'
-	END
-	ELSE IF (@salary BETWEEN 30000 AND 50000)
-	BEGIN
-		SET @Result = 'Average'
-	END
-	ELSE 
-	BEGIN
-		SET @Result = 'High'
-	END
-
-	RETURN @Result
-END
-
--- Problem 6.	E mployees by Salary Level
-CREATE PROCEDURE usp_EmployeesBySalaryLevel (@salaryLevel VARCHAR(10))
-AS
-BEGIN
-	SELECT e.FirstName, e.LastName FROM Employees AS e
-	WHERE dbo.ufn_GetSalaryLevel(e.Salary) = @salaryLevel
-END
-
--- Problem 7. Define Function
-CREATE FUNCTION ufn_IsWordComprised(@setOfLetters VARCHAR(MAX), @word VARCHAR(MAX)) 
-RETURNS TINYINT
-AS
-BEGIN
-	DECLARE @result TINYINT = 0;
-	IF (@word LIKE CONCAT(CONCAT('[',@setOfLetters),']'))
-	BEGIN
-	 SET @result = 1
-	END
-	ELSE
-	BEGIN
-	 SET @result = -1
-	END
-
-	RETURN @result
-END
-
-CREATE FUNCTION ufn_IsWordComprised(@setOfLetters VARCHAR(MAX), @word VARCHAR(MAX)) 
-RETURNS TINYINT
-AS
-BEGIN
-	DECLARE @result TINYINT = 1;
-	DECLARE @length INT = LEN(@word)
-	DECLARE @index INT = 1
-	DECLARE @char CHAR(1)
-
-	WHILE @index <= @length
-	BEGIN 
-	SET @char = SUBSTRING(@word, @index, 1)
-	IF(CHARINDEX(@char, @setOfLetters, 0) < 1)
-		BEGIN 
-			SET	@result = 0
+	RETURN
+		CASE 
+			WHEN @salary < 30000 THEN 'Low'
+			WHEN @salary BETWEEN 30000 AND 50000 THEN 'Average'
+			WHEN @salary > 50000 THEN 'High'
 		END
+END
 
-	SET @index = @index + 1
+SELECT Salary, dbo.ufn_GetSalaryLevel(Salary) AS 'Salary Level'
+FROM Employees
+
+-- 06. Employees by Salary Level
+CREATE PROCEDURE usp_EmployeesBySalaryLevel (@level NVARCHAR(10))
+AS 
+BEGIN
+	SELECT 
+		s.FirstName AS 'First Name',
+		s.LastName AS 'Last Name'
+	FROM (SELECT FirstName, LastName, Salary, 
+				dbo.ufn_GetSalaryLevel(Salary) AS sLevel
+			FROM Employees) s
+	WHERE s.sLevel = @level
+END
+ 
+EXEC usp_EmployeesBySalaryLevel Low
+
+-- 07. Define Function
+CREATE FUNCTION ufn_IsWordComprised (@setOfLetters NVARCHAR(20), @word NVARCHAR(20))
+RETURNS BIT
+AS
+BEGIN
+	DECLARE @comprised BIT = 1
+	DECLARE @index INT = 1
+ 	WHILE (@comprised = 1) AND (@index <= len(@word))
+	BEGIN
+		IF(CHARINDEX(LOWER(SUBSTRING(@word, @index, 1)), LOWER(@setOfLetters)) NOT BETWEEN 1 AND LEN(@setOfLetters))
+			BEGIN
+				SET @comprised = 0	
+			END
+		ELSE SET @index += 1
 	END
-	RETURN @result
+	RETURN @comprised
 END
 
--- Problem 9. Find Full Name
-CREATE PROCEDURE usp_GetHoldersFullName 
+SELECT dbo.ufn_IsWordComprised('pppp', 'Guy')
+
+-- 08. Delete Employees and Departments
+
+BEGIN TRANSACTION 
+ALTER TABLE EmployeesProjects
+DROP CONSTRAINT FK_EmployeesProjects_Employees
+ALTER TABLE Departments
+DROP CONSTRAINT FK_Departments_Employees
+ALTER TABLE Employees
+DROP CONSTRAINT FK_Employees_Employees
+DELETE FROM Employees
+WHERE DepartmentID IN (7,8)
+DELETE FROM Departments
+WHERE DepartmentID IN (7,8)
+
+ROLLBACK
+
+-- 09. Employees with Three Projects
+CREATE PROCEDURE usp_AssignProject(@emloyeeId INT, @projectID INT)
 AS
 BEGIN
-	SELECT FirstName + ' ' + LastName AS FullName FROM AccountHolders
+	BEGIN TRANSACTION
+		INSERT INTO EmployeesProjects
+			(EmployeeID, ProjectID)
+		VALUES
+			(@emloyeeId, @projectID)
+		IF (SELECT COUNT(EmployeeID) FROM EmployeesProjects
+			WHERE EmployeeID = @emloyeeId) > 3
+		BEGIN
+			RAISERROR('The employee has too many projects!', 16, 1)
+			ROLLBACK
+		END
+	COMMIT
 END
 
--- Problem 10.	People with Balance Higher Than
-CREATE PROCEDURE usp_GetHoldersWithBalanceHigherThan(@amount MONEY)
+EXEC usp_AssignProject 1, 6
+
+-- 10. Find Full Name
+CREATE PROCEDURE usp_GetHoldersFullName
 AS
 BEGIN
-	SELECT FirstName, LastName FROM AccountHolders AS ac 
-	LEFT JOIN Accounts AS a ON a.AccountHolderId = ac.Id 
-	GROUP BY FirstName, LastName
-	HAVING SUM(Balance) > @amount
+	SELECT FirstName + ' ' + LastName AS 'Full Name'
+	FROM AccountHolders
 END
 
--- Problem 11.	Future Value Function
-CREATE FUNCTION ufn_CalculateFutureValue (@sum MONEY, @interestRate FLOAT, @years FLOAT)
+EXEC usp_GetHoldersFullName
+
+-- 11. People with Balance Higher Than
+CREATE PROCEDURE usp_GetHoldersWithBalanceHigherThan(@number MONEY)
+AS
+BEGIN
+	SELECT FirstName AS 'First Name', LastName AS 'Last Name' 
+	FROM (SELECT FirstName, LastName,
+				SUM(a.Balance) AS TotalBalance 
+			FROM AccountHolders AS ah
+			JOIN Accounts AS a
+			ON a.AccountHolderId = ah.Id
+			GROUP BY ah.FirstName, ah.LastName
+		) AS tb
+	WHERE tb.TotalBalance > @number
+END
+
+EXEC dbo.usp_GetHoldersWithBalanceHigherThan 2 
+
+-- 12. Future Value Function
+CREATE FUNCTION ufn_CalculateFutureValue(@sum MONEY, @yir FLOAT, @ny INT)
 RETURNS MONEY
 AS
 BEGIN
-	DECLARE @totalMoney MONEY = @sum
-
-	SET @totalMoney = @totalMoney *  POWER((1 + @interestRate), @years)
-	RETURN @totalMoney
+	DECLARE @i INT = 1 
+	WHILE (@i <= @ny)
+	BEGIN
+		SET @sum = @sum + @sum*@yir
+		SET @i += 1
+	END
+	RETURN @sum
 END
 
 SELECT dbo.ufn_CalculateFutureValue(1000, 0.1, 5)
--- Problem 12. Calculating Interest
-CREATE PROCEDURE usp_CalculateFutureValueForAccount (@accountId FLOAT, @interestRate FLOAT)
+
+-- 13. Calculating Interest
+CREATE PROCEDURE usp_CalculateFutureValueForAccount(@accId INT, @rate FLOAT)
 AS
 BEGIN
-SELECT a.AccountId, ac.FirstName, ac.LastName, a.CurrentBalance, dbo.ufn_CalculateFutureValue(a.Balance, @interestRate, 5) AS BalanceIn5years FROM AccountHolders AS ac 
-		LEFT JOIN Accounts AS a ON a.AccountHolderId = ac.Id 
-		WHERE a.AccountHolderId = @accountId
+	SELECT ah.Id AS [Account Id], 
+		ah.FirstName AS [First Name],
+		ah.LastName AS [LastName],
+		a.Balance AS [Current Balanse], 
+		dbo.ufn_CalculateFutureValue(a.Balance, @rate, 5)
+	FROM AccountHolders ah
+	RIGHT JOIN Accounts a
+	ON ah.Id = a.AccountHolderId
+	WHERE a.Id = @accId
 END
 
--- Problem 13.	Deposit Money
+EXEC usp_CalculateFutureValueForAccount 1, 0.1
+
+-- 14. Deposit Money Procedure
 CREATE PROCEDURE usp_DepositMoney (@accountId INT, @moneyAmount MONEY)
 AS
 BEGIN
+	BEGIN TRANSACTION
 	UPDATE Accounts
 	SET Balance = Balance + @moneyAmount
-	WHERE Id = @accountId
-END
+	WHERE Accounts.Id = @AccountId
+	COMMIT 
+END 
 
--- Problem 14.	Withdraw Money
+EXEC usp_DepositMoney 1, 10000
+
+-- 15. Withdraw Money Procedure
 CREATE PROCEDURE usp_WithdrawMoney (@accountId INT, @moneyAmount MONEY)
 AS
 BEGIN
+	BEGIN TRANSACTION
 	UPDATE Accounts
 	SET Balance = Balance - @moneyAmount
-	WHERE Id = @accountId
+	WHERE Accounts.Id = @AccountId
+	COMMIT 
+END 
+
+EXEC usp_WithdrawMoney 1, 10000
+
+-- 16. Money Transfer
+CREATE PROCEDURE usp_TransferMoney(@senderId INT, @receiverId INT, @amount MONEY)
+AS
+BEGIN
+	BEGIN TRANSACTION
+	UPDATE Accounts
+	SET Balance = Balance - @amount
+	WHERE Accounts.Id = @senderId
+	UPDATE Accounts
+	SET Balance = Balance + @amount
+	WHERE Accounts.Id = @receiverId
+	IF (SELECT Balance FROM Accounts WHERE Accounts.Id = @senderId) < 0
+		ROLLBACK
+	ELSE COMMIT 
 END
 
--- Problem 19. Trigger
+EXEC usp_TransferMoney 1, 2, 10000
+
+-- 17. Create Table Logs
+
+--TODO
+
+-- Problem 20. Trigger
 CREATE TRIGGER tr_OnBuy ON UserGameItems
 INSTEAD OF INSERT
 AS 
@@ -170,23 +252,7 @@ BEGIN
 	LEFT JOIN GameTypeForbiddenItems as gtfi ON gtfi.GameTypeId = g.GameTypeId
 END
 
--- Problem 19.
-CREATE TRIGGER tr_OnBuy ON UserGameItems
-INSTEAD OF INSERT
-AS 
-BEGIN
-
-	INSERT INTO UserGameItems SELECT ItemId, UserGameId FROM inserted
-	WHERE ItemId IN (
-		SELECT Id FROM Items 
-		WHERE MinLevel <= (
-			SELECT ug.Level FROM UsersGames as ug
-				WHERE ug.GameId = UserGameId))
-END
-
-SELECT MinLevel FROM Items WHERE Id = 2
-
--- Problem 20. Massive Shopping
+-- Problem 21. Massive Shopping
 BEGIN TRAN
 DECLARE @totalItemSum MONEY = (SELECT SUM(Price) AS TotalPrice FROM Items AS i 
 		WHERE i.MinLevel BETWEEN 11 AND 12)
@@ -256,12 +322,11 @@ SELECT * FROM UserGameItems AS ugi
 								LEFT JOIN Users AS u ON u.Id = ug.UserId
 									WHERE u.FirstName = 'Stamat' AND g.Name = 'Safflower')
 
--- Problem 21. Number of Users for Email Provider
+-- Problem 22. Number of Users for Email Provider
 SELECT SUBSTRING(u.Email,CHARINDEX('@', u.Email, 0) + 1, LEN(u.Email)) AS 'Email Provider', COUNT(*) AS 'Number Of Users' FROM Users as u
 GROUP BY SUBSTRING(u.Email,CHARINDEX('@', u.Email, 0) + 1, LEN(u.Email))
 ORDER BY COUNT(*) DESC, 'Email Provider' ASC
-
--- Problem 22. All User in Games
+	- Problem 22. All User in Games
 SELECT g.Name AS Game, gt.Name AS 'Game Type', u.Username, ug.Level, ug.Cash, c.Name AS Character FROM Users AS u
 	INNER JOIN UsersGames AS ug ON ug.UserId = u.Id
 	LEFT JOIN Games AS g ON g.Id = ug.GameId
@@ -269,7 +334,7 @@ SELECT g.Name AS Game, gt.Name AS 'Game Type', u.Username, ug.Level, ug.Cash, c.
 	LEFT JOIN Characters AS c ON c.Id = ug.CharacterId
 	ORDER BY ug.Level DESC, u.Username ASC, Game ASC
 
--- Problem 23. Users in Games with Their Items
+-- Problem 24. Users in Games with Their Items
 SELECT u.Username, g.Name AS Game, COUNT(*) AS 'Items Count', SUM(i.Price) AS 'Items Price'
 		FROM Users AS u
 	INNER JOIN UsersGames AS ug ON ug.UserId = u.Id
@@ -280,7 +345,7 @@ SELECT u.Username, g.Name AS Game, COUNT(*) AS 'Items Count', SUM(i.Price) AS 'I
 	HAVING COUNT(*) >= 10
 	ORDER BY COUNT(*) DESC, SUM(i.Price) DESC, u.Username ASC
 
--- Problem 24. * User in Games with Their Statistics
+-- Problem 25. * User in Games with Their Statistics
 SELECT u.Username, g.Name AS Game,c.Name AS Character,  s.Strength , s.Defence, s.Speed, s.Mind, s.Luck FROM Users AS u
 	INNER JOIN UsersGames AS ug ON ug.UserId = u.Id
 	LEFT JOIN Games AS g ON g.Id = ug.GameId
@@ -288,7 +353,7 @@ SELECT u.Username, g.Name AS Game,c.Name AS Character,  s.Strength , s.Defence, 
 	LEFT JOIN [dbo].[Statistics] AS s ON s.Id = c.StatisticId
 	ORDER BY s.Strength DESC, s.Defence DESC, s.Speed DESC, s.Mind DESC, s.Luck DESC
 
--- Problem 25.	All Items with Greater than Average Statistics
+-- Problem 26.	All Items with Greater than Average Statistics
 SELECT i.Name, i.Price, i.MinLevel, s.Strength, s.Defence, s.Speed, s.Luck, s.Mind FROM Items AS i
 	INNER JOIN [dbo].[Statistics] AS s ON s.Id = i.StatisticId
 	WHERE s.Mind > (SELECT AVG(st.Mind) FROM [dbo].[Statistics] AS st) AND
@@ -296,13 +361,13 @@ SELECT i.Name, i.Price, i.MinLevel, s.Strength, s.Defence, s.Speed, s.Luck, s.Mi
 		s.Speed > (SELECT AVG(st.Speed) FROM [dbo].[Statistics] AS st)
 	ORDER BY i.Name
 
--- Problem 26.	Display All Items with information about Forbidden Game Type
+-- Problem 27.	Display All Items with information about Forbidden Game Type
 SELECT i.Name, i.Price, i.MinLevel, gt.Name AS 'Forbidden Game Type' FROM Items AS i
 	LEFT JOIN GameTypeForbiddenItems AS gtfi ON gtfi.ItemId = i.Id
 	LEFT JOIN GameTypes AS gt ON gt.Id = gtfi.GameTypeId
  ORDER BY gt.Name DESC, i.Name ASC
 
- -- Problem 27. 
+ -- Problem 28. 
 DECLARE @alexId INT = (SELECT Id FROM Users WHERE Username = 'Alex')
 DECLARE @gameId INT = (SELECT Id FROM Games WHERE Name = 'Edinburgh')
 DECLARE @userGameID INT = (SELECT Id FROM UsersGames WHERE UserId = @alexId AND GameId = @gameId)
@@ -331,12 +396,12 @@ SELECT u.Username, g.Name, ug.Cash, i.Name AS 'Item Name' FROM Users AS u
 	WHERE g.Id = 221
 	ORDER BY i.Name
 
--- Problem 28.
+-- Problem 29.
 SELECT p.PeakName, m.MountainRange, p.Elevation FROM Peaks AS p
 	INNER JOIN Mountains AS m ON m.Id = p.MountainId
 	ORDER BY p.Elevation DESC, p.PeakName ASC
 
--- Problem 29.
+-- Problem 30.
 SELECT p.PeakName, m.MountainRange AS Mountain, c.CountryName, con.ContinentName  FROM Peaks AS p
 	INNER JOIN Mountains AS m ON m.Id = p.MountainId
 	INNER JOIN MountainsCountries AS mc on mc.MountainId = m.Id
@@ -344,7 +409,7 @@ SELECT p.PeakName, m.MountainRange AS Mountain, c.CountryName, con.ContinentName
 	INNER JOIN Continents AS con ON con.ContinentCode = c.ContinentCode
 	ORDER BY p.PeakName ASC, c.CountryName ASC
 
--- Problem 30.
+-- Problem 31.
 SELECT co.CountryName, con.ContinentName,
 CASE
 	WHEN COUNT(r.Id) IS NULL THEN 0
@@ -363,19 +428,19 @@ FROM Countries AS co
 	GROUP BY co.CountryName, con.ContinentName
 	ORDER BY RiversCount DESC, TotalLength DESC, co.CountryName ASC
 
--- Problem 31.
+-- Problem 32.
 SELECT cu.CurrencyCode, cu.Description AS Currency, COUNT(co.CountryName) AS NumberOfCountries FROM Currencies AS cu 
  LEFT JOIN Countries AS co ON co.CurrencyCode = cu.CurrencyCode
  GROUP BY cu.CurrencyCode, cu.Description
  ORDER BY COUNT(co.CountryName) DESC, cu.Description ASC
 
--- Problem 32.
+-- Problem 33.
 SELECT con.ContinentName, SUM(cou.AreaInSqKm) AS CountriesArea, SUM(CAST(cou.Population AS BIGINT)) AS CountriesPopulation FROM Continents AS con
 	INNER JOIN Countries AS cou ON cou.ContinentCode = con.ContinentCode
 	GROUP BY con.ContinentName
 	ORDER BY SUM(CAST(cou.Population AS BIGINT)) DESC
 
--- Problem 33.
+-- Problem 34.
 CREATE TABLE Monasteries
 (
 Id INT PRIMARY KEY IDENTITY,
@@ -422,4 +487,4 @@ ALTER TABLE Countries
 ADD IsDeleted TINYINT
 CONSTRAINT D_Value DEFAULT 0
 
--- Problem 34.
+-- Problem 35.							
