107d0eb58e026e646c40fbcae14c3e16e09c9bc6
martinski74 <martin.dobr@abv.bg>
2017-06-02 09:26:17 +0300

    homework

---
+++ C#_Advanced-May2017/Matrices-Exercises/01.MatrixOfPalindromes/MatrixOfPalindromes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.MatrixOfPalindromes
+{
+    class MatrixOfPalindromes
+    {
+        static void Main()
+        {
+            var tokens = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+            var matrix = new string[tokens[0], tokens[1]];
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = row, colIndex = 0; colIndex < matrix.GetLength(1); col++, colIndex++)
+                {
+                    matrix[row, colIndex] = $"{alphabet[row]}{alphabet[col]}{alphabet[row]}";
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#_Advanced-May2017/Matrices-Exercises/02.DiagonalDifference/DiagonalDifference.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.DiagonalDifference
+{
+    class DiagonalDifference
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            }
+
+            //Primary diagonal:
+            int primarySum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                primarySum += matrix[row][row];
+            }
+
+            //Secondary diagonal:
+            int secondarySum = 0;
+
+            for (int row = 0, col = n - 1; row < n; row++, col--)
+            {
+                secondarySum += matrix[row][col];
+            }
+
+            Console.WriteLine(Math.Abs(primarySum - secondarySum));
+        }
+    }
+}
