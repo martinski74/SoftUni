33cc84acc5218fd3a2db608644dfd13e4d628ac9
martinski74 <martin.dobr@abv.bg>
2017-07-28 08:47:33 +0300

    solved.

---
+++ C#_OOP_Advanced/IteratorsAndComparators-Lab/01.Library/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public class Book 
+{
+    public Book(string title, int year, params string[] authors)
+    {
+        this.Title = title;
+        this.Year = year;
+        this.Authors =authors;
+    }
+    public string Title { get; set; }
+    public int Year { get; set; }
+    public IReadOnlyList<string> Authors { get; set; }
+
+    public int CompareTo(Book other)
+    {
+        if (this.Year.CompareTo(other.Year) != 0)
+        {
+            return this.Year.CompareTo(other.Year);
+        }
+        return this.Title.CompareTo(other.Title);
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Title} - {this.Year}";
+    }
+}
+
---
+++ C#_OOP_Advanced/IteratorsAndComparators-Lab/04.BookComparer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _04.BookComparer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Documents in the Case", 1930);
+        }
+    }
+}
