5df00fb4b8f1aef32ad069bcce781f9cbce924d3
martinski74 <martin.dobr@abv.bg>
2017-07-28 13:03:38 +0300

    solved.

---
+++ C#_OOP_Advanced/EnumsAndAttributes-Lab/02.CoffeMachine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace _02.CoffeMachine
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CoffeeMachine machine = new CoffeeMachine();
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] inputArgs = input.Split();
+                if (inputArgs.Length == 1)
+                {
+                    machine.InsertCoin(inputArgs[0]);
+                }
+                else
+                {
+                    machine.BuyCoffee(inputArgs[0], inputArgs[1]);
+                }
+            }
+
+            foreach (var coffeeType in machine.CoffeesSold)
+            {
+                Console.WriteLine(coffeeType);
+            }
+        }
+    }
+}
---
+++ C#_OOP_Advanced/EnumsAndAttributes-Lab/03.Attributes/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+[SoftUni("Ventsi")]
+
+public class StartUp
+{
+    [SoftUni("Gosho")]
+    public static void Main()
+    {
+        var tracker = new Tracker();
+        tracker.PrintMethodsByAuthor();
+    }
+}
+
