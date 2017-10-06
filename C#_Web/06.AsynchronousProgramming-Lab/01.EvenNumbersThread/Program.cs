8e726e45644c4cfde6db52593a2febeea6061f98
martinski74 <martin.dobr@abv.bg>
2017-10-06 09:15:38 +0300

    homework

---
+++ C#_Web/06.AsynchronousProgramming-Lab/01.EvenNumbersThread/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _01.EvenNumbersThread
+{
+    using System;
+    using System.Threading;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var min = int.Parse(Console.ReadLine());
+            var max = int.Parse(Console.ReadLine());
+
+            Thread evens = new Thread(() => PrintEvenNumbes(min,max));
+            evens.Start();
+            evens.Join();
+            Console.WriteLine("Thread finished work");
+
+        }
+       
+        private static void PrintEvenNumbes(int min, int max)
+        {
+            for (int i = min; i <= max; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ C#_Web/06.AsynchronousProgramming-Lab/02.SliceFile/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _02.SliceFile
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var fileName = "music.mp3";
+            var destination = "Pieces";
+            var pieces = 5;
+
+            SliceAsync(fileName,destination,pieces);
+            Console.WriteLine("Anythink else?");
+           
+                Console.WriteLine();
+                
+        }
+        private static void Slice(string sourceFile, string destinationPath, int parts)
+        {
+            if (!Directory.Exists(destinationPath))
+            {
+                Directory.CreateDirectory(destinationPath);
+            }
+
+            using (FileStream source = new FileStream(sourceFile, FileMode.Open))
+            {
+                FileInfo fileInfo = new FileInfo(sourceFile);
+                long partLength = source.Length / parts + 1;
+                long currentByte = 0;
+
+                for (int currentPart = 0; currentPart < parts; currentPart++)
+                {
+                    string filePath = $"{destinationPath}/Part-{currentPart}{fileInfo.Extension}";
+
+                    using (FileStream destination = new FileStream(filePath, FileMode.Create))
+                    {
+                        byte[] buffer = new byte[1024];
+                        while (currentByte <= partLength * currentPart)
+                        {
+                            int readBytesCount = source.Read(buffer, 0, buffer.Length);
+                            if (readBytesCount == 0)
+                            {
+                                break;
+                            }
+
+                            destination.Write(buffer, 0, readBytesCount);
+                            currentByte += readBytesCount;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static async void SliceAsync(string sourceFile, string destinationParh, int parts)
+        {
+            await Task
+                .Run(() => Slice(sourceFile, destinationParh, parts));
+        }
+
+
+
+    }
+}
