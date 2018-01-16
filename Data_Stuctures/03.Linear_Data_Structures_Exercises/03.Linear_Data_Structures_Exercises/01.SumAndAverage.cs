cdd80e80a9485a3d55e83a46cea1a339a07615fd
martinski74 <martin.dobr@abv.bg>
2018-01-16 17:37:07 +0200

    homework

---
+++ Data_Stuctures/03.Linear_Data_Structures_Exercises/03.Linear_Data_Structures_Exercises/01.SumAndAverage.cs
@@ -0,0 +1,25 @@
+﻿namespace _03.Linear_Data_Structures_Exercises
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SumAndAverage
+    {
+        public static void Main()
+        {
+            var resultList = new List<int>();
+           
+
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            foreach (var item in input)
+            {
+                resultList.Add(item);
+            }
+            var sum = resultList.Sum();
+            var avg = resultList.Average();
+            Console.WriteLine($"Sum={sum}; Average={avg:f2}");
+        }
+    }
+}
---
+++ Data_Stuctures/03.Linear_Data_Structures_Exercises/04.RemoveOddOccurrencs/RemoveOddOccurrencs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.RemoveOddOccurrencs
+{
+    public class RemoveOddOccurrencs
+    {
+        public static void Main()
+        {
+            var resultList = new List<int>();
+
+            var numbers = Console.ReadLine()
+                                .Split(' ')
+                                .Select(int.Parse).ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int i1 = numbers[i];
+                if (numbers
+                    .Where(x => x == i1)
+                    .ToList()
+                    .Count() % 2 == 0)
+                {
+                    resultList.Add(i1);
+                }
+            }
+            Console.WriteLine(string.Join(" ",resultList));
+        }
+    }
+}
